1fe2a3f003dde0aff83ec7d07f4fb7a4300d850f
Jonathon Baxendell <j.bax626@gmail.com>
2022-10-03 13:10:25 -0500

    detail back button router setup to go back to list

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,9 +1,13 @@
 import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 
 function Details () {
 
+    const history = useHistory();
+
     const movie = useSelector(store => store.details);
+
 
     return (
         <>
@@ -11,6 +15,7 @@ function Details () {
             <h2>{movie.title}</h2>
             <img src={movie.poster} alt={movie.title}/>
             <h4>{movie.description}</h4>
+            <button onClick={() => history.push('/')}>Back to List</button>
         </>
 
     );
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -2,19 +2,21 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './MovieList.css'
 import { useHistory } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
+import Details from '../Details/Details';
 
 function MovieList() {
 
     const dispatch = useDispatch();
     const movies = useSelector(store => store.movies);
     const history = useHistory();
-   
+
 
     useEffect(() => {
         dispatch({ type: 'FETCH_MOVIES' });
     }, []);
 
-    function selectDetails (movie) {
+    function selectDetails(movie) {
         console.log('Movie Id:', movie.id);
 
         dispatch({ type: 'SELECT_MOVIE', payload: movie });
@@ -23,8 +25,8 @@ function MovieList() {
     }
 
     return (
-        <main>
-            
+        <>
+
             <h1>MovieList</h1>
             <section className="movies">
                 {movies.map(movie => {
@@ -36,7 +38,7 @@ function MovieList() {
                     );
                 })}
             </section>
-        </main>
+        </>
 
     );
 }
